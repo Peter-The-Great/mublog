d1d1a94acc56db94b45b7d22a8642e875d8511b2
Kees van Voorthuizen <me@keesvv.nl>
2023-11-13 12:08:21 +0100

    feat: style user content

--- src/lib/UserContent.svelte
+++ src/lib/UserContent.svelte
@@ -10,4 +10,56 @@
   div :global(img) {
     @apply rounded-md my-5;
   }
+
+  div :global(a) {
+    @apply underline;
+  }
+
+  div :global(strong) {
+    @apply font-semibold;
+  }
+
+  div :global(h1),
+  div :global(h2),
+  div :global(h3) {
+    @apply mt-5;
+  }
+
+  div :global(h1),
+  div :global(h2) {
+    @apply text-2xl font-semibold;
+  }
+
+  div :global(h3) {
+    @apply text-lg detail;
+  }
+
+  div :global(blockquote) {
+    @apply border-l-2 px-4 py-2 my-2 detail italic;
+  }
+
+  div :global(ul) {
+    @apply list-disc list-inside;
+  }
+
+  div :global(ol) {
+    @apply list-decimal list-inside;
+  }
+
+  div :global(thead) {
+    @apply text-left;
+  }
+
+  div :global(th) {
+    @apply font-semibold;
+  }
+
+  div :global(th),
+  div :global(td) {
+    @apply border px-2 py-0.5;
+  }
+
+  div :global(pre) {
+    @apply text-xs;
+  }
 </style>
